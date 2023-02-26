1bbae057deb4b09c4b3927e44aeb5c27f2999ba8
Othreumaru <odrinwhite@gmail.com>
2023-02-26 19:12:22 +0000

    added card back

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,6 +2,7 @@ import { useLoader } from "@react-three/fiber";
 import { forwardRef } from "react";
 import { Euler, Mesh, TextureLoader, Vector3 } from "three";
 import cardMiddleSrc from "./assets/card-middle.png";
+import cardBackSrc from "./assets/card-back.png";
 
 export type Props = {
   cardSize: number;
@@ -12,11 +13,21 @@ export type Props = {
 
 export const Card = forwardRef<Mesh, Props>(
   ({ cardSize = 4, cardThickness = 0.1, ...restProps }, ref) => {
-    const colorMap = useLoader(TextureLoader, cardMiddleSrc);
+    const colorMapMiddle = useLoader(TextureLoader, cardMiddleSrc);
+    const colorMapBack = useLoader(TextureLoader, cardBackSrc);
     return (
       <mesh ref={ref} {...restProps}>
         <boxGeometry args={[0.7 * cardSize, cardSize, cardThickness]} />
-        <meshStandardMaterial map={colorMap} transparent={true} />
+        <meshStandardMaterial
+          map={colorMapMiddle}
+          transparent={true}
+          attach="material-4"
+        />
+        <meshStandardMaterial
+          map={colorMapBack}
+          transparent={true}
+          attach="material-5"
+        />
       </mesh>
     );
   }
